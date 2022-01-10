6340560f96414eee06879cdc0c172b0155fc63fa
YoungHun <younghun4218@gmail.com>
2022-01-10 19:55:10 +0900

    터미네이터 시청 중 이스터에그추가

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
+		client.user.setActivity('터미네이터', { type: 'WATCHING' });
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };

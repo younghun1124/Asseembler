7f33329120d42f492d2ccb6a558c34666ae2401d
YoungHun <younghun4218@gmail.com>
2022-01-11 20:49:06 +0900

    쓸데없는 consolelog 삭제

--- events/customFunctions/getUserFromMention.js
+++ events/customFunctions/getUserFromMention.js
@@ -2,10 +2,7 @@ const {
 	MessageMentions: { USERS_PATTERN },
 } = require('discord.js');
 module.exports = function getUserFromMention(mention) {
-	console.log(mention);
 	// The id is the first and only match found by the RegEx.
-	// console.log('유저패턴: ' + USERS_PATTERN);
-
 	const matches = mention.match(USERS_PATTERN);
 
 	// If supplied variable was not a mention, matches will be null instead of an array.

1635c74f0b6de1bead7eeec274ffa063eb3b084c
Hun-X1 <younghun4218@gmail.com>
2022-01-17 16:41:02 +0900

    멘션에서 id 값만을 받아오도록 수정

--- events/customFunctions/getUserIdsFromMention.js
+++ events/customFunctions/getUserIdsFromMention.js
@@ -1,12 +1,16 @@
 const {
 	MessageMentions: { USERS_PATTERN },
 } = require('discord.js');
-module.exports = function getUserFromMention(mention) {
+module.exports = function getUserIdsFromMention(mention) {
 	// The id is the first and only match found by the RegEx.
 	const matches = mention.match(USERS_PATTERN);
 
 	// If supplied variable was not a mention, matches will be null instead of an array.
 	if (!matches) return;
-
-	return matches;
+	const eraseMentionTag = (tag) => {
+		return tag.slice(2, -1);
+	};
+	const ids = matches.map(eraseMentionTag);
+	console.log(ids);
+	return ids;
 };
